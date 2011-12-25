595ec7074ba2d019f5d145df55fa3a11ae6d8ac0
jaipratik <jp_ca@live.com>
2011-12-25 13:42:02 -0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guard2::Application.config.secret_token = '6404ec321a71e6ff56c798c1396b8d7596f49837955d14a706f1e0774cb18522c97e21efeecda5d94c22f959d3747264c507280c4f1b8c27d6571a2a46b04983'
